f93d3128b0dc0d652c22e684e292c2ab390d21e9
Tristan-Guerro <33086546+Tristan-Guerro@users.noreply.github.com>
2020-02-04 13:56:39 +0100

    Add files via upload

--- src/components/bdd/bddrequest.js
+++ src/components/bdd/bddrequest.js
@@ -1,50 +1,48 @@
-var Connection = require('tedious').Connection;  
-    var config = {  
-        server: 'xlstatlog.database.windows.net',
-        authentication: {
-            type: 'default',
-            options: {
-                userName: 'xlstat',
-                password: 'addinsoft123!'
-            }
-        },
-        options: {
-            encrypt: true,
-            database: 'xlstat_releaseNote_Copy'
-        }
-    }; 
-    var connection = new Connection(config);  
-    connection.on('connect', function(err) {    
-        console.log("Connected");  
-        executeStatement();  
-    });  
-  
-    var Request = require('tedious').Request;  
-    var TYPES = require('tedious').TYPES;  
-  
-    executeStatement = () => {  
-        request = new Request("select COUNT(id) from campagne where enabled=1;", function(err) {  
-        if (err) {  
-            console.log(err);}
-        });  
-        var result = "";  
-        request.on('row', function(columns) {  
-            columns.forEach(function(column) {  
-              if (column.value === null) {  
-                console.log('NULL');  
-              } else {  
-                result+= column.value + " ";  
-              }  
-            });  
-            console.log(result);  
-            result ="";  
-        });  
-  
-        request.on('done', function(rowCount, more) {  
-        console.log(rowCount + ' rows returned');  
-        });  
-        connection.execSql(request);  
-    }
+const Connection = require('tedious').Connection;  
 
-export var Connection;
-export function executeStatement();
+export const config = {  
+    server: 'xlstatlog.database.windows.net',
+    authentication: {
+        type: 'default',
+        options: {
+            userName: 'xlstat',
+            password: 'addinsoft123!'
+        }
+    },
+    options: {
+        encrypt: true,
+        database: 'xlstat_releaseNote_Copy'
+    }
+}; 
+
+const Request = require('tedious').Request;  
+
+const executeStatement = () => {  
+    request = new Request("select COUNT(id) from campagne where enabled=1;", function(err) {  
+    if (err) {  
+        console.log(err);}
+    });  
+    let result = "";  
+    request.on('row', function(columns) {  
+        columns.forEach(function(column) {  
+            if (column.value === null) {  
+            console.log('NULL');  
+            } else {  
+                result += column.value + " ";  
+            }  
+        });  
+        console.log(result);  
+        result = "";  
+    });  
+
+    request.on('done', function(rowCount, more) {  
+        console.log(rowCount + ' rows returned');  
+    });  
+    connection.execSql(request);  
+}
+
+export const connection = new Connection(config);  
+connection.on('connect', function(err) {    
+    console.log("Connected");  
+    executeStatement();  
+}); 
